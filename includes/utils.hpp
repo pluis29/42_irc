6827e946600b1bff557e8cc44c5cb4e4233ebe7e
Luis <lp_santos29@outlook.com>
2023-08-23 17:53:31 -0300

    add part command refactor quit and who

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -8,7 +8,7 @@ void error_message(std::string function, std::string error);
 std::vector<std::string> split(std::string str, char c);
 std::string toUpperCase(std::string str);
 bool check_invalid_char(std::string str);
-
+std::string joinToString(std::vector<std::string>::iterator first, std::vector<std::string>::iterator last);
 }  // namespace Utils
 
 #endif // UTILS_HPP_
